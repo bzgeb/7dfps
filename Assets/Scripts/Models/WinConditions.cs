60ad9720de35b064c32f8cbb105af8904e4d9931
Bronson Zgeb <bronson.zgeb@gmail.com>
2013-08-15 13:19:46 -0400

    Added win conditions with win events that don't do anything

---
+++ Assets/Scripts/Models/WinConditions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinConditions : MonoBehaviour {
+    public Score gathererScore;
+    public float gathererLives;
+    public float scoreToWin;
+
+    void OnEnable() {
+        EventManager.Register( "OnKilled", OnKilled );
+        EventManager.Register( "OnStashedObject", OnStashedObject );
+    }
+
+    void OnDisable() {
+        EventManager.Deregister( "OnKilled", OnKilled );
+        EventManager.Deregister( "OnStashedObject", OnStashedObject );
+    }
+
+    void OnKilled( params object[] args ) {
+        --gathererLives;
+
+        if ( gathererLives < 1 ) {
+            EventManager.Push( "OnHunterWins" );
+        }
+    }
+
+    void OnStashedObject( params object[] args ) {
+        float stash = gathererScore.Stash;
+
+        if ( stash >= scoreToWin ) {
+            EventManager.Push( "OnGathererWins" ); 
+        }
+    }
+}
